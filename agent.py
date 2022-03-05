a8072ae1800ed7548f370bb9fd6a24343532d7dc
Impossibum <86384743+Impossibum@users.noreply.github.com>
2022-03-05 13:12:21 -0500

    converted for use with sb3-rlgym  tutorial

---
+++ agent.py
@@ -0,0 +1,25 @@
+import numpy as np
+from stable_baselines3 import PPO
+import pathlib
+from parsers.discrete_act import DiscreteAction
+
+
+class Agent:
+    def __init__(self):
+        _path = pathlib.Path(__file__).parent.resolve()
+        custom_objects = {
+            "lr_schedule": 0.000001,
+            "clip_range": .02,
+            "n_envs": 1,
+            "device": "cpu"
+        }
+
+        self.actor = PPO.load(str(_path) + '/example_mdl', custom_objects=custom_objects)
+        self.parser = DiscreteAction()
+
+
+    def act(self, state):
+        action = self.actor.predict(state, deterministic=True)
+        x = self.parser.parse_actions(action[0], state)
+
+        return x[0]
--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 import numpy as np
 from agent import Agent
-from obs.default_obs import DefaultObs
+from obs.advanced_obs import AdvancedObs
 from rlgym_compat import GameState
 
 
@@ -13,7 +13,7 @@ class RLGymExampleBot(BaseAgent):
 
         # FIXME Hey, botmaker. Start here:
         # Swap the obs builder if you are using a different one, RLGym's AdvancedObs is also available
-        self.obs_builder = DefaultObs()
+        self.obs_builder = AdvancedObs()
         # Your neural network logic goes inside the Agent class, go take a look inside src/agent.py
         self.agent = Agent()
         # Adjust the tickskip if your agent was trained with a different value
@@ -25,7 +25,10 @@ class RLGymExampleBot(BaseAgent):
         self.update_action = True
         self.ticks = 0
         self.prev_time = 0
-        print('RLGymExampleBot Ready - Index:', index)
+        self.expected_teammates = 0
+        self.expected_opponents = 1
+        print(f'{self.name} Ready - Index:', index)
+
 
     def initialize_agent(self):
         # Initialize the rlgym GameState object now that the game is active and the info is available
@@ -36,31 +39,29 @@ class RLGymExampleBot(BaseAgent):
         self.action = np.zeros(8)
         self.update_action = True
 
+    def reshape_state(self, gamestate, player, opponents, allies):
+        """ TODO - replace me with code that handles different sized teams
+        - converting to 1v1 currently """
+        closest_op = min(opponents, key=lambda p: np.linalg.norm(self.game_state.ball.position - p.car_data.position))
+        self.game_state.players = [player, closest_op]
+
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         cur_time = packet.game_info.seconds_elapsed
         delta = cur_time - self.prev_time
         self.prev_time = cur_time
 
-        ticks_elapsed = delta // 0.008  # Smaller than 1/120 on purpose
+        ticks_elapsed = delta * 120
         self.ticks += ticks_elapsed
         self.game_state.decode(packet, ticks_elapsed)
 
         if self.update_action:
             self.update_action = False
-
-            # FIXME Hey, botmaker. Verify that this is what you need for your agent
-            # By default we treat every match as a 1v1 against a fixed opponent,
-            # by doing this your bot can participate in 2v2 or 3v3 matches. Feel free to change this
             player = self.game_state.players[self.index]
             opponents = [p for p in self.game_state.players if p.team_num != self.team]
+            allies = [p for p in self.game_state.players if p.team_num == self.team and p.car_id != self.index]
 
-            # Another option is to focus on the opponent closest to the ball
-            # you can use any logic you see fit to choose the op you want to focus on
-            closest_op = min(opponents, key=lambda p: np.linalg.norm(self.game_state.ball.position - p.car_data.position))
-            # self.renderer.draw_string_3d(closest_op.car_data.position, 2, 2, "CLOSEST", self.renderer.white())
-
-            # Here we are are rebuilding the player list as if the match were a 1v1
-            self.game_state.players = [player, opponents[0]]
+            if len(opponents) != self.expected_opponents or len(allies) != self.expected_teammates:
+                self.reshape_state(self.game_state, player, opponents, allies)
 
             obs = self.obs_builder.build_obs(player, self.game_state, self.action)
             self.action = self.agent.act(obs)
