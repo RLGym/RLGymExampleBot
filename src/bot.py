289841318b474c74f5540f36af3afb8ff52cbe96
Lucas Emery <lucas.emery@hotmail.com>
2022-03-27 16:59:07 +0200

    Dodge fix, rlbot action delay fix, action parsers and new default obs

--- src/bot.py
+++ src/bot.py
@@ -2,6 +2,8 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 import numpy as np
+
+from action.default_act import DefaultAction
 from agent import Agent
 from obs.default_obs import DefaultObs
 from rlgym_compat import GameState
@@ -14,6 +16,8 @@ class RLGymExampleBot(BaseAgent):
         # FIXME Hey, botmaker. Start here:
         # Swap the obs builder if you are using a different one, RLGym's AdvancedObs is also available
         self.obs_builder = DefaultObs()
+        # Swap the action parser if you are using a different one, RLGym's Discrete and Continuous are also available
+        self.act_parser = DefaultAction()
         # Your neural network logic goes inside the Agent class, go take a look inside src/agent.py
         self.agent = Agent()
         # Adjust the tickskip if your agent was trained with a different value
@@ -41,7 +45,7 @@ class RLGymExampleBot(BaseAgent):
         delta = cur_time - self.prev_time
         self.prev_time = cur_time
 
-        ticks_elapsed = delta // 0.008  # Smaller than 1/120 on purpose
+        ticks_elapsed = round(delta * 120)
         self.ticks += ticks_elapsed
         self.game_state.decode(packet, ticks_elapsed)
 
@@ -52,22 +56,30 @@ class RLGymExampleBot(BaseAgent):
             # By default we treat every match as a 1v1 against a fixed opponent,
             # by doing this your bot can participate in 2v2 or 3v3 matches. Feel free to change this
             player = self.game_state.players[self.index]
+            teammates = [p for p in self.game_state.players if p.team_num == self.team]
             opponents = [p for p in self.game_state.players if p.team_num != self.team]
 
-            # Another option is to focus on the opponent closest to the ball
-            # you can use any logic you see fit to choose the op you want to focus on
-            closest_op = min(opponents, key=lambda p: np.linalg.norm(self.game_state.ball.position - p.car_data.position))
-            # self.renderer.draw_string_3d(closest_op.car_data.position, 2, 2, "CLOSEST", self.renderer.white())
+            if len(opponents) == 0:
+                # There's no opponent, we assume this model is 1v0
+                self.game_state.players = [player]
+            else:
+                # Sort by distance to ball
+                teammates.sort(key=lambda p: np.linalg.norm(self.game_state.ball.position - p.car_data.position))
+                opponents.sort(key=lambda p: np.linalg.norm(self.game_state.ball.position - p.car_data.position))
 
-            # Here we are are rebuilding the player list as if the match were a 1v1
-            self.game_state.players = [player, opponents[0]]
+                # Grab opponent in same "position" relative to it's teammates
+                opponent = opponents[min(teammates.index(player), len(opponents) - 1)]
+
+                self.game_state.players = [player, opponent]
 
             obs = self.obs_builder.build_obs(player, self.game_state, self.action)
-            self.action = self.agent.act(obs)
+            self.action = self.act_parser.parse_actions(self.agent.act(obs), self.game_state)[0]  # Dim is (N, 8)
+
+        if self.ticks >= self.tick_skip - 1:
+            self.update_controls(self.action)
 
         if self.ticks >= self.tick_skip:
             self.ticks = 0
-            self.update_controls(self.action)
             self.update_action = True
 
         return self.controls
@@ -76,7 +88,7 @@ class RLGymExampleBot(BaseAgent):
         self.controls.throttle = action[0]
         self.controls.steer = action[1]
         self.controls.pitch = action[2]
-        self.controls.yaw = action[3]
+        self.controls.yaw = 0 if action[5] > 0 else action[3]
         self.controls.roll = action[4]
         self.controls.jump = action[5] > 0
         self.controls.boost = action[6] > 0
